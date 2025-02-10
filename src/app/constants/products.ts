e1cea705cba5d0dfa6edf2b200ab258147b69fdb
Cesar <cesar.hertz@icloud.com>
2025-02-10 22:14:14 +0100

    Add product constants for seafood offerings with details and availability

---
+++ src/app/constants/products.ts
@@ -0,0 +1,110 @@
+export const products = [
+  {
+    id: 1,
+    name: "Saumon Fumé Entier",
+    img: "saumon-fume-entier-1.jpg",
+    price: 65,
+    description:
+      "Délicatement fumé au bois de chêne traditionnel, notre saumon est un témoignage du savoir-faire artisanal.",
+    available: true,
+  },
+  {
+    id: 2,
+    name: "Saumon Fumé Prétranché",
+    img: "saumon-fume-pretranche-1.jpg",
+    price: 65,
+    description:
+      "Soigneusement préparée avec notre mélange signature d'herbes et d'épices, puis doucement fumée à la perfection.",
+    available: true,
+  },
+  {
+    id: 3,
+    name: "Coeur de saumon",
+    img: "coeur-saumon-1.png",
+    price: 25,
+    description:
+      "Ce morceau épais de cœur de saumon est à la fois moelleux, tendre et fondant. A couper en lamelles ou en cubes et à déguster sans modération ! Délicieux accompagné de câpres à queue.",
+    available: true,
+  },
+  {
+    id: 4,
+    name: "Saumon fumé pavé",
+    img: "saumon-fume-pave-1.webp",
+    price: 32,
+    description: "Vide",
+    available: true,
+  },
+  {
+    id: 5,
+    name: "Saumon Gravlax Entier",
+    img: "saumon-gravad-entier-1.jpg",
+
+    price: 28.99,
+    description:
+      "Mariné selon une recette traditionnelle scandinave, notre gravlax est un équilibre délicat de saveurs.",
+    available: true,
+  },
+  {
+    id: 6,
+    name: "Saumon Gravad Prétranché",
+    img: "saumon-gravad-pretranche-1.jpg",
+    price: 27.99,
+    description:
+      "Mariné façon gravlax où sel, poivres, sucre et aneth se mêlent en un parfait accord pour un saumon relevé",
+    available: true,
+  },
+  {
+    id: 7,
+    name: "Saumon Fumé à Chaud au Poivre Entier",
+    img: "saumon-fume-chaud-poivre-entier-1.jpg",
+    price: 26.99,
+    description:
+      "Fumé à chaud pour conserver l'humidité, puis fini avec un enrobage de poivre noir concassé pour un coup de piquant.",
+    available: true,
+  },
+  {
+    id: 8,
+    name: "Saumon Fumé à Chaud Poivre Pavé",
+    img: "saumon-fume-chaud-nature.jpg",
+    price: 26.99,
+    description:
+      "Notre saumon fumé à chaud classique, préparé pour laisser les saveurs naturelles du poisson s'exprimer pleinement.",
+    available: true,
+  },
+  {
+    id: 9,
+    name: "Saumon Fumé à Chaud Nature Pavé",
+    img: "saumon-fume-chaud-nature.jpg",
+    price: 26.99,
+    description:
+      "Notre saumon fumé à chaud classique, préparé pour laisser les saveurs naturelles du poisson s'exprimer pleinement.",
+    available: true,
+  },
+  {
+    id: 10,
+    name: "Truite Arc-en-ciel",
+    img: "truite-1.jpg",
+    price: 17.99,
+    description:
+      "Provenant des ruisseaux locaux, notre truite est nettoyée et filetée à la main pour une saveur des plus fraîches.",
+    available: true,
+  },
+  {
+    id: 11,
+    name: "Filet de Flétan",
+    img: "fletan-1.jpg",
+    price: 65,
+    description:
+      "Notre flétan est lentement fumé pour rehausser ses saveurs naturelles, résultant en un goût riche et beurré.  Coupé à la perfection, nos steaks de flétan témoignent de la compétence de nos maîtres poissonniers.",
+    available: true,
+  },
+  {
+    id: 12,
+    name: "Crevettes Groenland",
+    img: "crevettes-1.jpg",
+    price: 32.99,
+    description:
+      "Soigneusement sélectionnées et préparées, nos crevettes sont un délice qui témoigne de notre engagement envers la qualité.",
+    available: true,
+  },
+];
