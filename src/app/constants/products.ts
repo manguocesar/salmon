4de6775b0d96d3a78a92cd7cc3899672a559b4b4
Cesar <cesar.hertz@icloud.com>
2025-02-10 22:30:29 +0100

    Add detailed descriptions for seafood products and enhance Contact page content

--- src/app/constants/products.ts
+++ src/app/constants/products.ts
@@ -7,6 +7,8 @@ export const products = [
     description:
       "Délicatement fumé au bois de chêne traditionnel, notre saumon est un témoignage du savoir-faire artisanal.",
     available: true,
+    details:
+      "Filet de saumon fumé entier. Origine : Norvège. Poids : env 1kg. Salmo salar. Fumé au bois de hêtre dans notre atelier à Copenhague. Produit entier non tranché – avec peau. Produit frais – peut être congelé. DLC : Entre 7 et 20 jours. (même si on sait qu’il sera engloutit bien avant…) Conditionné sous vide pour une meilleure conservation",
   },
   {
     id: 2,
@@ -16,6 +18,8 @@ export const products = [
     description:
       "Soigneusement préparée avec notre mélange signature d'herbes et d'épices, puis doucement fumée à la perfection.",
     available: true,
+    details:
+      "Saumon fumé Prétranché. 4 paquets de 250g. Origine : Norvège. Poids : 1kg (4*250g). Salmo salar. Fumé au bois de hêtre dans notre atelier à Copenhague. Sans peau. Produit frais – peut être congelé DLC : 10 jours après ouverture ou sortie du congelateur (même si on sait qu’il sera engloutit bien avant…) Conditionné sous vide pour une meilleure conservation",
   },
   {
     id: 3,
@@ -25,6 +29,8 @@ export const products = [
     description:
       "Ce morceau épais de cœur de saumon est à la fois moelleux, tendre et fondant. A couper en lamelles ou en cubes et à déguster sans modération ! Délicieux accompagné de câpres à queue.",
     available: true,
+    details:
+      "Barquette de 200g. Le Cœur de saumon est issu de la partie la plus charnue : le meilleur du saumon. Origine : Norvège. Conditions d'utilisation : Ouvrir le sachet 20 mn avant de consommer, à consommer sous 5 jours après ouverture en conservant au froid.",
   },
   {
     id: 4,
@@ -33,16 +39,19 @@ export const products = [
     price: 32,
     description: "Vide",
     available: true,
+    details:
+      "Le fumage à froid (entre 22 et 24°c) au bois de hêtre se fait de façon verticale et traditionnelle. Cette technique permet d'étirer les fibres du poisson, ce qui lui procure une texture plus sèche et un goût de fumage plus prononcé. Cette technique permet également d'éliminer une partie plus importante des graisses (3 à 4%) par rapport à un fumage classique. Il est tranché à la main afin de mieux respecter sa chair si caractéristique.",
   },
   {
     id: 5,
     name: "Saumon Gravlax Entier",
     img: "saumon-gravad-entier-1.jpg",
-
     price: 28.99,
     description:
       "Mariné selon une recette traditionnelle scandinave, notre gravlax est un équilibre délicat de saveurs.",
     available: true,
+    details:
+      "Le gravlax vous invite à découvrir le saumon autrement. Recette d'origine scandinave : poisson frais légèrement salé et sucré, puis mariné à l'huile d'olive avec des baies roses et de l'aneth. Rajoutez un savoir-faire artisanal acquis depuis plus de 40 ans, un saumon de haute qualité... et vous obtenez une vraie gourmandise pour les amoureux du saumon et de son fondant !",
   },
   {
     id: 6,
@@ -52,6 +61,8 @@ export const products = [
     description:
       "Mariné façon gravlax où sel, poivres, sucre et aneth se mêlent en un parfait accord pour un saumon relevé",
     available: true,
+    details:
+      "Le gravlax vous invite à découvrir le saumon autrement. Recette d'origine scandinave : poisson frais légèrement salé et sucré, puis mariné à l'huile d'olive avec des baies roses et de l'aneth. Rajoutez un savoir-faire artisanal acquis depuis plus de 40 ans, un saumon de haute qualité... et vous obtenez une vraie gourmandise pour les amoureux du saumon et de son fondant !",
   },
   {
     id: 7,
@@ -61,6 +72,8 @@ export const products = [
     description:
       "Fumé à chaud pour conserver l'humidité, puis fini avec un enrobage de poivre noir concassé pour un coup de piquant.",
     available: true,
+    details:
+      "Son extrémité est caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter dans une salade ou des pâtes.",
   },
   {
     id: 8,
@@ -70,6 +83,8 @@ export const products = [
     description:
       "Notre saumon fumé à chaud classique, préparé pour laisser les saveurs naturelles du poisson s'exprimer pleinement.",
     available: true,
+    detils:
+      "Son extrémité est caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter en salade",
   },
   {
     id: 9,
@@ -79,24 +94,30 @@ export const products = [
     description:
       "Notre saumon fumé à chaud classique, préparé pour laisser les saveurs naturelles du poisson s'exprimer pleinement.",
     available: true,
+    detils:
+      "Son extrémité est caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter en salade",
   },
   {
     id: 10,
-    name: "Truite Arc-en-ciel",
-    img: "truite-1.jpg",
-    price: 17.99,
-    description:
-      "Provenant des ruisseaux locaux, notre truite est nettoyée et filetée à la main pour une saveur des plus fraîches.",
-    available: true,
-  },
-  {
-    id: 11,
     name: "Filet de Flétan",
     img: "fletan-1.jpg",
     price: 65,
     description:
       "Notre flétan est lentement fumé pour rehausser ses saveurs naturelles, résultant en un goût riche et beurré.  Coupé à la perfection, nos steaks de flétan témoignent de la compétence de nos maîtres poissonniers.",
     available: true,
+    details:
+      "Le filet de flétan fumé a une chair de couleur blanche. Les arômes légers de notre fumage préservent la saveur iodée et la texture légère et fondant",
+  },
+  {
+    id: 11,
+    name: "Truite Arc-en-ciel",
+    img: "truite-1.jpg",
+    price: 17.99,
+    description:
+      "Provenant des ruisseaux locaux, notre truite est nettoyée et filetée à la main pour une saveur des plus fraîches.",
+    available: true,
+    details:
+      "Légère et savoureuse, la truite fumée peut se servir froide ou chaude. Elle convient pour 2 personnes en petite entrée, ou pour une personne en plat ou entrée copieuse. Vous pouvez aussi présenter une entrée mixte, composée d’un filet de truite fumée et d’une tranche de saumon fumé.",
   },
   {
     id: 12,
@@ -106,5 +127,7 @@ export const products = [
     description:
       "Soigneusement sélectionnées et préparées, nos crevettes sont un délice qui témoigne de notre engagement envers la qualité.",
     available: true,
+    details:
+      "Les crevettes sont cuites et décortiquées. Elles sont ensuite fumées à froid avec du bois de hêtre. Les crevettes fumées sont ensuite conditionnées sous vide pour une meilleure conservation. Les crevettes fumées sont à conserver au frais entre 0 et 4°C. Elles se conservent 10 jours après ouverture.",
   },
 ];
