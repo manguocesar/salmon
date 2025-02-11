0bf10d2b8450d1fa850d568e80afabbee4fe9c21
Cesar <cesar.hertz@icloud.com>
2025-02-11 22:59:24 +0100

    Add Cancel and Success pages, implement quantity update in CartContext, and enhance product details

---
+++ src/app/cancel/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cancel = () => {
+    return (
+        <div className="text-black">
+            <h2>L'achat n'a pas eu lieu, veuillez réessayer</h2>
+        </div>
+    );
+}
+
+export default Cancel
