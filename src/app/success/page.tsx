54a7212a7a260abe4b4c13e3ab26171cd215e149
Cesar <cesar.hertz@icloud.com>
2025-02-14 10:25:42 +0100

    Refactor success page layout for improved styling and user experience; update contact email and button text

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,42 +1,32 @@
 'use client';
 
 import Link from 'next/link';
-
-import { useCart } from '../contexts/CartContext';
 import { runFireworks } from '../lib/utils';
 import { useEffect } from 'react';
 import Image from 'next/image';
 
 const Success = () => {
-    const { } = useCart();
-
     useEffect(() => {
-        // localStorage.clear();
-        // setCartItems([]);
-        // setTotalPrice(0);
-        // setTotalQuantities(0);
+        localStorage.clear();
         runFireworks();
     }, []);
 
     return (
-        <div className="min-h-60 text-black">
-            <div className="w-9/12 flex justify-center items-center flex-col mx-auto mt-20">
-                <Image className="rounded-xl" src="/logo.jpg" alt="Mikael Hertz Poissons Fumés Artisanaux" width={200} height={80} />
-
-                <h2>Merci pour votre commande!</h2>
-                <p className="email-msg">Verifiez vos emails pour la commande.</p>
-                <p className="description">
-                    Si vous avez des questions, n'hésitez pas à nous contacter:
-                    <a className="email" href="mailto:cesar.hertz@icloud.com">
-                        cesar.hertz@icloud.com
-                    </a>
-                </p>
-                <Link href="/">
-                    <button type="button" style={{ width: '300px' }} className="btn">
-                        Continuer à Shopping
-                    </button>
-                </Link>
-            </div>
+        <div className="min-h-60 text-2xl text-black w-9/12 flex justify-center items-center flex-col mx-auto mt-20">
+            <Image className="m-4 rounded-xl" src="/logo.jpg" alt="Mikael Hertz Poissons Fumés Artisanaux" width={300} height={200} />
+            <h2>Merci pour votre commande!</h2>
+            <p className="email-msg">Verifiez vos emails pour la commande.</p>
+            <p className="description">
+                Si vous avez des questions, n'hésitez pas à nous contacter:
+            </p>
+            <a className="font-bold m-3" href="mailto:mikaelhertz@me.com">
+                mikaelhertz@me.com
+            </a>
+            <Link href="/">
+                <button type="button" className="bg-blue-700 text-white p-3 rounded-lg hover:opacity-70 m-5 text-3xl">
+                    Retour à l'Accueil
+                </button>
+            </Link>
         </div>
     )
 }
