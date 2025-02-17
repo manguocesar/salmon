3f30587379bdf971ed41468d89f727d292c5b1cb
Cesar <cesar.hertz@icloud.com>
2025-02-17 23:17:02 +0100

    Add saumonGravadEntierUrls constant for product image references

---
+++ src/app/constants/saumonGravadEntierUrls.ts
@@ -0,0 +1,9 @@
+export const saumonGravadEntierUrls = [
+  "saumon-gravad-entier-1.jpg",
+  "saumon-gravad-entier-2.jpg",
+  "saumon-gravad-entier-3.jpg",
+  "saumon-gravad-entier-4.jpg",
+  "saumon-gravad-entier-5.jpg",
+  "saumon-gravad-entier-6.jpg",
+  "saumon-gravad-entier-7.webp",
+];
