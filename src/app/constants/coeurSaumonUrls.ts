964d74a83775d0649a12c2ea830828e8030727ad
Cesar <cesar.hertz@icloud.com>
2025-02-17 23:16:44 +0100

    Add coeurSaumonUrls constant for product image references

---
+++ src/app/constants/coeurSaumonUrls.ts
@@ -0,0 +1,10 @@
+export const coeurSaumonUrls = [
+  "coeur-saumon-1.png",
+  "coeur-saumon-2.jpg",
+  "coeur-saumon-3.jpg",
+  "coeur-saumon-4.jpg",
+  "coeur-saumon-5.jpg",
+  "coeur-saumon-6.webp",
+  "coeur-saumon-7.webp",
+  "coeur-saumon-8.jpg",
+];
