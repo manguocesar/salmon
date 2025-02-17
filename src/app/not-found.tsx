0a0652ec13edd56e9dc6d450e2687c57ce287cab
Cesar <cesar.hertz@icloud.com>
2025-02-17 20:53:18 +0100

    Make NotFound component asynchronous for improved data handling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
+'use server';
 import Link from 'next/link';
 
-export default function NotFound() {
+export default async function NotFound() {
     return (
         <main className="flex h-full flex-col items-center justify-center gap-2">
             <h2 className="text-xl font-semibold">Page inaccessible</h2>
