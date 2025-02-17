9a811e337ae76a05bb903afc0aee4ba7c55a5575
Cesar <cesar.hertz@icloud.com>
2025-02-17 20:54:22 +0100

    Refactor ArtisanalSelection component to be asynchronous and update Home component to use it without props

--- src/app/components/ArtisanalSelection.tsx
+++ src/app/components/ArtisanalSelection.tsx
@@ -1,13 +1,16 @@
-import { Product } from "../types/products";
+'use server';
 import { ProductCard } from "./ProductCard";
+import { products } from "../constants/products"
 
-export const ArtisanalSelection = ({ products }: { products: Product[] }) => (
-    <div className="p-2 md:p-4 rounded-lg">
-        <h2 className="text-3xl">Sélection Artisanale</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
-    </div >
-)
+export const ArtisanalSelection = async () => {
+    return (
+        <div className="p-2 md:p-4 rounded-lg">
+            <h2 className="text-3xl">Sélection Artisanale</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div >
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-import { products } from "./constants/products"
+'use server';
+
 import { DiscoverProducts } from "./components/DiscoverProducts"
 import { ArtisanalPromise } from "./components/ArtisanalPromise"
 import { ArtisanalSelection } from "./components/ArtisanalSelection"
 import { ArtisanalProcessus } from "./components/ArtisanalProcessus"
+import { ProductSlider } from "./components/ProductSlider";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="space-y-4 md:space-y-8 text-black">
       <DiscoverProducts />
       <ArtisanalProcessus />
-      <ArtisanalSelection products={products} />
+      <ArtisanalSelection />
+      <ProductSlider />
       <ArtisanalPromise />
     </div>
   )
