f6898ab55f255ff376a3734e6dca04954b510886
Cesar <cesar.hertz@icloud.com>
2025-02-17 20:54:01 +0100

    Refactor ArtisanalProcessus, ArtisanalPromise, and DiscoverProducts components to be asynchronous for improved data handling

--- src/app/components/ArtisanalProcessus.tsx
+++ src/app/components/ArtisanalProcessus.tsx
@@ -1,22 +1,25 @@
+'use server';
 
-export const ArtisanalProcessus = () => (
-    <div className="bg-gray-100 p-2 md:p-4 rounded-lg">
-        <h2 className="text-3xl">Un Processus Artisanal:</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-8 mt-2 md:mt-3">
-            <div>
-                <h3 className="text-2xl">Techniques Traditionnelles</h3>
-                <p className="text-gray-700 md:mb-4">
-                    Un processus de fumage perfectionné au fil des générations, utilisant des méthodes
-                    traditionnelles qui font ressortir les meilleures saveurs de chaque filet de poisson.
-                </p>
+export const ArtisanalProcessus = async () => {
+    return (
+        <div className="bg-gray-100 p-2 md:p-4 rounded-lg" >
+            <h2 className="text-3xl">Un Processus Artisanal:</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-8 mt-2 md:mt-3">
+                <div>
+                    <h3 className="text-2xl">Techniques Traditionnelles</h3>
+                    <p className="text-gray-700 md:mb-4">
+                        Un processus de fumage perfectionné au fil des générations, utilisant des méthodes
+                        traditionnelles qui font ressortir les meilleures saveurs de chaque filet de poisson.
+                    </p>
+                </div>
+                <div>
+                    <h3 className="text-2xl">Excellence Artisanale</h3>
+                    <p className="text-gray-700 md:mb-4">
+                        Chaque produit est préparé à la main, garantissant la plus haute qualité et une attention aux détails dans
+                        chaque tranche et filet.
+                    </p>
+                </div>
             </div>
-            <div>
-                <h3 className="text-2xl">Excellence Artisanale</h3>
-                <p className="text-gray-700 md:mb-4">
-                    Chaque produit est préparé à la main, garantissant la plus haute qualité et une attention aux détails dans
-                    chaque tranche et filet.
-                </p>
-            </div>
-        </div>
-    </div>
-)
+        </div >
+    )
+}
--- src/app/components/ArtisanalPromise.tsx
+++ src/app/components/ArtisanalPromise.tsx
@@ -1,12 +1,14 @@
-export const ArtisanalPromise = () => (
-    <div className="bg-gray-100 p-2 md:p-4 rounded-lg">
-        <h2 className="text-3xl mb-3">La Promesse Artisanale</h2>
-        <ul className="list-disc pl-6 space-y-2">
-            <li>Poissons de qualité premium & traditionel</li>
-            <li>Techniques de fumage traditionnelles transmises de génération en génération</li>
-            <li>Préparation à la main avec une attention méticuleuse aux détails</li>
-            <li>Pratiques d'élevage durables pour préserver un savoir-faire pour les générations futures</li>
-            <li>Livraison disponible aux alentours de Lyon avant Noël et Pâques</li>
-        </ul>
-    </div>
-)
+export const ArtisanalPromise = () => {
+    return (
+        <div className="bg-gray-100 p-2 md:p-4 rounded-lg">
+            <h2 className="text-3xl mb-3">La Promesse Artisanale</h2>
+            <ul className="list-disc pl-6 space-y-2">
+                <li>Poissons de qualité premium & traditionel</li>
+                <li>Techniques de fumage traditionnelles transmises de génération en génération</li>
+                <li>Préparation à la main avec une attention méticuleuse aux détails</li>
+                <li>Pratiques d'élevage durables pour préserver un savoir-faire pour les générations futures</li>
+                <li>Livraison disponible aux alentours de Lyon avant Noël et Pâques</li>
+            </ul>
+        </div>
+    )
+}
