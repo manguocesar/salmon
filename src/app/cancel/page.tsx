f0c92a3aabbf2b510f17f19c59d0a67ab696cd68
Cesar <cesar.hertz@icloud.com>
2025-02-18 19:58:20 +0100

    Add ProductSlider component to Cancel page for enhanced product display

--- src/app/cancel/page.tsx
+++ src/app/cancel/page.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 import React from 'react'
+import { products } from '../constants/products';
+import { ProductSlider } from '../components/ProductSlider';
 
 const Cancel = () => {
     return (
         <div className="text-black">
             <h2 className='my-3 text-2xl'>L'achat n'a pas eu lieu, veuillez réessayer</h2>
-
             <Link href="/" className="text-orange-600 font-bold text-xl hover:underline">Retour à la page d'accueil</Link>
+            <ProductSlider urlRoot="products" products={products} />
         </div>
     );
 }
