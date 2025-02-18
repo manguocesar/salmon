7f606b0926826a24bd0a48cb21185b7a99bd15aa
Cesar <cesar.hertz@icloud.com>
2025-02-18 10:02:01 +0100

    Refactor ArtisanalProcessus and DiscoverProducts components for improved layout and styling

--- src/app/components/DiscoverProducts.tsx
+++ src/app/components/DiscoverProducts.tsx
@@ -6,11 +6,11 @@ export const DiscoverProducts = async () => {
 
     return (
         <div className="text-center p-2 md:p-4 rounded-lg">
-            <h1 className="text-4xl">Poissons Fumés Artisanalement</h1>
-            <p className="text-2xl m-8">Saumons, truites et flétans exquis</p>
+            <h1 className="text-orange-500 text-4xl">Poissons Fumés Artisanalement</h1>
+            <p className="text-2xl font-semibold m-6">Saumons, truites et flétans exquis</p>
             <Link
                 href="/products"
-                className="bg-orange-600  text-white p-5 rounded-lg md:text-3xl font-semibold hover:bg-orange-700 transition-all duration-300"
+                className="bg-orange-600 font-bold text-xl text-white p-5 rounded-lg md:text-3xl md:font-semibold hover:bg-orange-700 transition-all duration-300"
             >
                 Découvrir Nos Produits
             </Link>
