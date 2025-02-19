a0eb91a631e151f8be2c0761e0ca4cd7a74e61be
Cesar <cesar.hertz@icloud.com>
2025-02-19 14:07:57 +0100

    Update product descriptions for clarity and conciseness

--- src/app/constants/products.ts
+++ src/app/constants/products.ts
@@ -6,7 +6,7 @@ export const products = [
     img: "saumon-fume-entier-1.jpg",
     price: 65,
     description:
-      "Délicatement fumé au bois de hetre traditionnel, un saumon témoignage du savoir-faire artisanal.",
+      "Délicatement fumé au bois de hetre traditionnel, un saumon témoin de savoir-faire artisanal.",
     available: true,
     details:
       "Filet de saumon fumé entier. Saumon élevé en Norvège, fumé au Danmark.. Poids : env 1kg. Salmo salar. Fumé au bois de hêtre à l'atelier au Danmark. Le saumon fumé entier est idéal pour les grandes tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Le fumage à froid (entre 22 et 24°c) au bois de hêtre de façon traditionnelle. Cette technique permet d'étirer les fibres du poisson, ce qui lui procure une texture plus sèche et un goût de fumage plus prononcé. Cette technique permet également d'éliminer une partie plus importante des graisses (3 à 4%) par rapport à un fumage classique. Il est tranché à la main afin de mieux respecter sa chair si caractéristique.",
@@ -19,8 +19,7 @@ export const products = [
     name: "B - Saumon Fumé Prétranché",
     img: "saumon-fume-pretranche-1.jpg",
     price: 65,
-    description:
-      "Saumon fumé préparation prétranché, doucement fumée à la perfection.",
+    description: "Fines tranches, doucement fumées à la perfection.",
     available: true,
     details:
       "Saumon fumé Prétranché. 4 paquets de 250g. Saumon élevé en Norvège, fumé au Danmark.. Poids : 1kg (4*250g). Salmo salar. Fumé au bois de hêtre à l'atelier au Danmark. Sans peau. Le saumon fumé prétranché est idéal pour les petites tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Il est prêt à être dégusté, il ne reste plus qu’à le sortir de son emballage et à le disposer sur un plateau. Le fumage à froid (entre 22 et 24°c) au bois de hêtre de façon traditionnelle. Cette technique permet d'étirer les fibres du poisson, ce qui lui procure une texture plus sèche et un goût de fumage plus prononcé. Cette technique permet également d'éliminer une partie plus importante des graisses (3 à 4%) par rapport à un fumage classique. Il est tranché à la main afin de mieux respecter sa chair si caractéristique.",
@@ -62,7 +61,7 @@ export const products = [
     img: "saumon-gravad-entier-2.jpg",
     price: 65,
     description:
-      "Mariné selon une recette traditionnelle scandinave, notre gravlax est un équilibre délicat de saveurs.",
+      "Recette traditionnelle scandinave, un équilibre délicat de saveurs.",
     available: true,
     details:
       "Le gravlax filet entier vous invite à découvrir le saumon autrement. Filet entier env. 1 kg. Saumon élevé en Norvège, fumé au Danmark. Recette d'origine scandinave : poisson frais légèrement salé et sucré, puis mariné à l'huile d'olive avec des baies roses et de l'aneth. Rajoutez un savoir-faire artisanal acquis depuis plus de 40 ans, un saumon de haute qualité... et vous obtenez une vraie gourmandise pour les amoureux du saumon et de son fondant! Le gravlax est idéal pour les grandes tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Il est prêt à être dégusté, il ne reste plus qu’à le sortir de son emballage et à le disposer sur un plateau.",
@@ -76,7 +75,7 @@ export const products = [
     img: "saumon-gravad-pretranche-1.jpg",
     price: 65,
     description:
-      "Mariné façon gravlax où sel, poivres, sucre et aneth se mêlent en un parfait accord pour un saumon relevé",
+      "Sel, poivres, sucre et aneth en accord parfait sur fines tranches.",
     available: true,
     details:
       "Le gravlax prétranché vous invite à découvrir le saumon autrement. Saumon élevé en Norvège, fumé au Danmark. 4 paquets pretranchés de 250gr. Recette d'origine scandinave : poisson frais légèrement salé et sucré, puis mariné à l'huile d'olive avec des baies roses et de l'aneth. Rajoutez un savoir-faire artisanal acquis depuis plus de 40 ans, un saumon de haute qualité... et vous obtenez une vraie gourmandise pour les amoureux du saumon et de son fondant !",
@@ -90,7 +89,7 @@ export const products = [
     img: "saumon-fume-chaud-poivre-entier-1.avif",
     price: 65,
     description:
-      "Fumé à chaud pour conserver l'humidité, puis fini avec un enrobage de poivre noir concassé pour un coup de piquant.",
+      "Mi-cuit pour conserver l'humidité, puis enrobé de poivre noir concassé.",
     available: true,
     details:
       "Filet entier environ 1kg. Saumon élevé en Norvège, fumé au Danmark. Son extrémité est légerement caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter dans une salade ou des pâtes. Le saumon fumé à chaud au poivre entier est idéal pour les grandes tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Il est prêt à être dégusté, il ne reste plus qu’à le sortir de son emballage et à le disposer sur un plateau.",
@@ -132,7 +131,7 @@ export const products = [
     img: "fletan-1.jpg",
     price: 65,
     description:
-      "Flétan lentement fumé pour rehausser ses saveurs naturelles, résultant en un goût riche et beurré.",
+      "Flétan lentement fumé pour rehausser ses saveurs naturelles pour un goût riche.",
     available: true,
     details:
       "Le filet de flétan fumé a une chair de couleur blanche. Les arômes légers de notre fumage préservent la saveur iodée et la texture légère et fondant. Coupé à la perfection, nos steaks de flétan témoignent de la compétence de nos maîtres poissonniers.",
@@ -145,7 +144,7 @@ export const products = [
     name: "K - Truites Fumées",
     img: "truite-1.jpeg",
     price: 25,
-    description: "Truite fumée d'elvage nettoyée et filetée à la main.",
+    description: "Truite fumée d'elvage nettoyée et filetée manuellement.",
     available: true,
     details:
       "Légère et savoureuse, la truite fumée peut se servir froide ou chaude. 4 paquets de 125g. Elle convient pour 2 personnes en petite entrée, ou pour une personne en plat ou entrée copieuse. Vous pouvez aussi présenter une entrée mixte, composée d’un filet de truite fumée et d’une tranche de saumon fumé.",
@@ -159,7 +158,7 @@ export const products = [
     img: "crevettes-1.jpg",
     price: 15,
     description:
-      "Soigneusement sélectionnées et préparées, nos crevettes sont un délice qui témoigne de notre engagement envers la qualité.",
+      "Soigneusement sélectionnées et préparées, délicieuses crevettes témoins de qualité.",
     available: true,
     details:
       "Les crevettes sont cuites et décortiquées. 450g net. Elles sont ensuite fumées à froid avec du bois de hêtre. Les crevettes fumées sont ensuite conditionnées sous vide pour une meilleure conservation. Les crevettes fumées sont à conserver au frais entre 0 et 4°C. Elles se conservent 10 jours après ouverture.",
