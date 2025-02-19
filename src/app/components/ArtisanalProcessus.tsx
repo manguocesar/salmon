d8f1ca727a0f574cb0742430b7c4bc60fee3336f
Cesar <cesar.hertz@icloud.com>
2025-02-19 13:24:00 +0100

    Refactor ArtisanalProcessus component to improve structure and readability of content

--- src/app/components/ArtisanalProcessus.tsx
+++ src/app/components/ArtisanalProcessus.tsx
@@ -1,22 +1,22 @@
 'use server';
+import React from 'react';
+
+const Section = ({ title, children }: { title: string; children: React.ReactNode }) => (
+    <div>
+        <h3 className="text-2xl text-orange-500">{title}</h3>
+        <p className="text-gray-700 md:mb-4">{children}</p>
+    </div>
+);
 
 export const ArtisanalProcessus = async () => {
     return (
         <div className="bg-gray-100 p-2 md:p-4 rounded-lg grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-8 mt-2 md:mt-3">
-            <div>
-                <h3 className="text-2xl text-orange-500">Techniques Traditionnelles</h3>
-                <p className="text-gray-700 md:mb-4">
-                    Un processus de fumage perfectionné au fil des générations, utilisant des méthodes
-                    traditionnelles qui font ressortir les meilleures saveurs de chaque filet de poisson.
-                </p>
-            </div>
-            <div>
-                <h3 className="text-2xl text-orange-500">Excellence Artisanale</h3>
-                <p className="text-gray-700 md:mb-4">
-                    Chaque produit est préparé à la main, garantissant la plus haute qualité et une attention aux détails dans
-                    chaque tranche et filet.
-                </p>
-            </div>
+            <Section title="Techniques Traditionnelles:">
+                Fumage perfectionné au fil des générations, utilisant des méthodes traditionnelles faisant ressortir les meilleures saveurs de chaque poisson.
+            </Section>
+            <Section title="Excellence Artisanale:">
+                Préparé à la main, haute qualité & attention aux détails pour chaque filet.
+            </Section>
         </div>
     );
 };
