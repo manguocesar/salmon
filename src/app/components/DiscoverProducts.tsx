f2be40177ac6eae4414d6229a1732ad934688ed8
Cesar <cesar.hertz@icloud.com>
2025-02-20 22:32:51 +0100

    Remove SVGAnimation component from DiscoverProducts for cleaner presentation

--- src/app/components/DiscoverProducts.tsx
+++ src/app/components/DiscoverProducts.tsx
@@ -1,13 +1,9 @@
 'use server';
 
 import Link from "next/link";
-import { SVGAnimation } from "./SVGAnimation";
-
 export const DiscoverProducts = async () => {
-
     return (
         <div className="text-center md:my-4 md:p-4 rounded-lg">
-            <SVGAnimation />
             <h1 className="text-orange-600 mx-4 text-2xl font-semibold md:text-4xl ">Poissons Artisanaux</h1>
             <Link href="/products" >
                 <p className="duration-300 bg-orange-500 w-10/12 mx-auto md:w-6/12 md:mx-auto font-bold text-xl text-white p-5 m-2 rounded-lg md:text-3xl md:font-semibold hover:bg-orange-700 transition-all">Découvrir Nos Produits</p>
