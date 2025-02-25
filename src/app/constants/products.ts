f8cee855a2395c0a8869aa27ce681ae1bb3368c7
Cesar <cesar.hertz@icloud.com>
2025-02-25 16:24:03 +0100

    Product displayed updated

--- src/app/constants/products.ts
+++ src/app/constants/products.ts
@@ -66,23 +66,23 @@ export const products = [
     consumption:
       'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé.',
   },
-  {
-    id: 5,
-    url: 'saumonGravadEntier',
-    name: 'E - Saumon Mariné (gravad lax)',
-    shortName: 'E - Mariné Entier',
-    img: 'saumon-gravad-entier-2.jpg',
-    imgUrl: 'saumonGravadEntier/saumon-gravad-entier-2.jpg',
-    video: '',
-    price: 65,
-    description:
-      'Recette traditionnelle scandinave, un filet délicatement équilibré.',
-    available: true,
-    details:
-      "Le gravlax filet entier vous invite à découvrir le saumon autrement. Gravlax (saumon enterré) est issu du mot scandinave gräva/grave « creuser » et lax/laks, « saumon ». Filet entier env. 1 kg. Saumon élevé en Norvège, fumé au Danmark. Recette d'origine scandinave : poisson frais légèrement salé et sucré, puis mariné à l'huile d'olive avec des baies roses et de l'aneth. Rajoutez un savoir-faire artisanal acquis depuis plus de 40 ans, un saumon de haute qualité... et vous obtenez une vraie gourmandise pour les amoureux du saumon et de son fondant! Le gravlax est idéal pour les grandes tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Il est prêt à être dégusté, il ne reste plus qu’à le sortir de son emballage et à le disposer sur un plateau.",
-    consumption:
-      'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
-  },
+  // {
+  //   id: 5,
+  //   url: 'saumonGravadEntier',
+  //   name: 'E - Saumon Mariné (gravad lax)',
+  //   shortName: 'E - Mariné Entier',
+  //   img: 'saumon-gravad-entier-2.jpg',
+  //   imgUrl: 'saumonGravadEntier/saumon-gravad-entier-2.jpg',
+  //   video: '',
+  //   price: 65,
+  //   description:
+  //     'Recette traditionnelle scandinave, un filet délicatement équilibré.',
+  //   available: true,
+  //   details:
+  //     "Le gravlax filet entier vous invite à découvrir le saumon autrement. Gravlax (saumon enterré) est issu du mot scandinave gräva/grave « creuser » et lax/laks, « saumon ». Filet entier env. 1 kg. Saumon élevé en Norvège, fumé au Danmark. Recette d'origine scandinave : poisson frais légèrement salé et sucré, puis mariné à l'huile d'olive avec des baies roses et de l'aneth. Rajoutez un savoir-faire artisanal acquis depuis plus de 40 ans, un saumon de haute qualité... et vous obtenez une vraie gourmandise pour les amoureux du saumon et de son fondant! Le gravlax est idéal pour les grandes tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Il est prêt à être dégusté, il ne reste plus qu’à le sortir de son emballage et à le disposer sur un plateau.",
+  //   consumption:
+  //     'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
+  // },
   {
     id: 6,
     url: 'saumonGravadPretranche',
@@ -100,24 +100,24 @@ export const products = [
     consumption:
       'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
   },
-  {
-    id: 7,
-    url: 'saumonFumeChaudPoivreEntier',
-    name: 'G - Saumon Mi-Cuit Poivre',
-    shortName: 'G - Cuit Poivre Entier',
-    img: 'saumon-fume-chaud-poivre-entier-1.avif',
-    imgUrl:
-      'saumonFumeChaudPoivreEntier/saumon-fume-chaud-poivre-entier-1.avif',
-    video: '/saumonFumeChaudPoivrePave/fumeChaud.mp4',
-    price: 60,
-    description:
-      'Fumer à plus de 50° pour un goût marqué puis enrobé de poivre noir concassé.',
-    available: true,
-    details:
-      'Plus feuilleté et a une couleur plus claire que le saumon fumé à froid. Filet entier environ 1kg. Saumon élevé en Norvège, fumé au Danmark. Son extrémité est légerement caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter dans une salade ou des pâtes. Le saumon fumé à chaud au poivre entier est idéal pour les grandes tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Il est prêt à être dégusté, il ne reste plus qu’à le sortir de son emballage et à le disposer sur un plateau.',
-    consumption:
-      'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
-  },
+  // {
+  //   id: 7,
+  //   url: 'saumonFumeChaudPoivreEntier',
+  //   name: 'G - Saumon Mi-Cuit Poivre',
+  //   shortName: 'G - Cuit Poivre Entier',
+  //   img: 'saumon-fume-chaud-poivre-entier-1.avif',
+  //   imgUrl:
+  //     'saumonFumeChaudPoivreEntier/saumon-fume-chaud-poivre-entier-1.avif',
+  //   video: '/saumonFumeChaudPoivrePave/fumeChaud.mp4',
+  //   price: 60,
+  //   description:
+  //     'Fumer à plus de 50° pour un goût marqué puis enrobé de poivre noir concassé.',
+  //   available: true,
+  //   details:
+  //     'Plus feuilleté et a une couleur plus claire que le saumon fumé à froid. Filet entier environ 1kg. Saumon élevé en Norvège, fumé au Danmark. Son extrémité est légerement caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter dans une salade ou des pâtes. Le saumon fumé à chaud au poivre entier est idéal pour les grandes tablées, les buffets, les brunchs, les fêtes de famille ou les réceptions. Il est aussi parfait pour les traiteurs, les restaurateurs et les hôtels. Il est prêt à être dégusté, il ne reste plus qu’à le sortir de son emballage et à le disposer sur un plateau.',
+  //   consumption:
+  //     'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
+  // },
   {
     id: 8,
     url: 'saumonFumeChaudPoivrePave',
@@ -135,87 +135,87 @@ export const products = [
     consumption:
       'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
   },
-  {
-    id: 9,
-    url: 'saumonFumeChaudNaturePave',
-    name: 'I - Saumon Mi-Cuit Nature Pavé',
-    shortName: 'I - Cuit Nature',
-    img: 'saumon-fume-chaud-nature.jpg',
-    imgUrl: 'saumonFumeChaudNaturePave/saumon-fume-chaud-nature.jpg',
-    video: '',
-    price: 48,
-    description:
-      "Fumé à chaud Nature, laissant les saveurs naturelles du poisson s'exprimer pleinement.",
-    available: true,
-    details:
-      'Quatre paqués de 200grammes. 800 grammes. Saumon élevé en Norvège, fumé au Danmark. Son extrémité est caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter en salade',
-    consumption:
-      'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
-  },
-  {
-    id: 10,
-    url: 'fletan',
-    name: 'J - Flétan Fumé Prétranché',
-    shortName: 'J - Flétan',
-    img: 'fletan-1.jpg',
-    imgUrl: 'fletan/fletan-1.jpg',
-    video: '',
-    price: 65,
-    description:
-      'Lentement fumé rehaussant ses saveurs naturelles pour un goût riche.',
-    available: true,
-    details:
-      'Le filet de flétan fumé a une chair de couleur blanche. Les arômes légers de notre fumage préservent la saveur iodée et la texture légère et fondant. Coupé à la perfection, nos steaks de flétan témoignent de la compétence de nos maîtres poissonniers.',
-    consumption:
-      'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
-  },
-  {
-    id: 11,
-    url: 'truite',
-    name: 'K - Truites Fumées',
-    shortName: 'K - Truites',
-    img: 'truite-1.jpeg',
-    imgUrl: 'truite/truite-1.jpeg',
-    video: '',
-    price: 24,
-    description: "Truite fumée au bois d'hêtre puis filetée manuellement.",
-    available: true,
-    details:
-      "Légère et savoureuse, la truite fumée peut se servir froide ou chaude. 4 paquets de 125g. Elle convient pour 2 personnes en petite entrée, ou pour une personne en plat ou entrée copieuse. Vous pouvez aussi présenter une entrée mixte, composée d’un filet de truite fumée et d’une tranche de saumon fumé. Nom latin: Oncorhynchus mykiss. Fermes certifiées ASC soucieuses de l'environnement et du bien-être animal Anciennes méthodes de fumage sur copeaux de bois de hêtre. Aucune utilisation d’additifs – uniquement du sel",
-    consumption:
-      'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. Prêt à manger et pratique pour un déjeuner rapide ou un repas chaud. Peut être dégusté froid ou chaud. Excellente source de protéines',
-  },
-  {
-    id: 12,
-    url: 'crevettes',
-    name: 'L - Crevettes Décortiquées',
-    shortName: 'L - Crevettes',
-    img: 'crevettes-1.jpg',
-    imgUrl: 'crevettes/crevettes-1.jpg',
-    video: '/crevettes/crevettes.mp4',
-    price: 18,
-    description:
-      'Soigneusement sélectionnées et préparées, délicieuses crevettes témoins de qualité.',
-    available: true,
-    details:
-      'Les crevettes sont cuites et décortiquées. 450g net. Elles sont ensuite fumées à froid avec du bois de hêtre. Les crevettes fumées sont ensuite conditionnées sous vide pour une meilleure conservation. Les crevettes fumées sont à conserver au frais entre 0 et 4°C. Elles se conservent 10 jours après ouverture.',
-    consumption:
-      'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
-  },
-  {
-    id: 13,
-    url: 'couteau',
-    name: 'M - Couteau Saumon',
-    shortName: 'M - Couteau',
-    img: 'couteau.png',
-    imgUrl: 'couteau/couteau.png',
-    video: '',
-    price: 5,
-    description:
-      'Couteau à saumon. Lame arrondie pour une coupe nette et précise.',
-    available: true,
-    details:
-      'Couteau à saumon. Lame arrondie pour une coupe nette et précise. Longueure 30cm.',
-    consumption: '',
-  },
+  // {
+  //   id: 9,
+  //   url: 'saumonFumeChaudNaturePave',
+  //   name: 'I - Saumon Mi-Cuit Nature Pavé',
+  //   shortName: 'I - Cuit Nature',
+  //   img: 'saumon-fume-chaud-nature.jpg',
+  //   imgUrl: 'saumonFumeChaudNaturePave/saumon-fume-chaud-nature.jpg',
+  //   video: '',
+  //   price: 48,
+  //   description:
+  //     "Fumé à chaud Nature, laissant les saveurs naturelles du poisson s'exprimer pleinement.",
+  //   available: true,
+  //   details:
+  //     'Quatre paqués de 200grammes. 800 grammes. Saumon élevé en Norvège, fumé au Danmark. Son extrémité est caramélisée, avec un flan plus ou moins gras et une partie très charnue. Cette partie du poisson, fondante en bouche, est parfaite à manger seule ou à ajouter en salade',
+  //   consumption:
+  //     'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
+  // },
+  // {
+  //   id: 10,
+  //   url: 'fletan',
+  //   name: 'J - Flétan Fumé Prétranché',
+  //   shortName: 'J - Flétan',
+  //   img: 'fletan-1.jpg',
+  //   imgUrl: 'fletan/fletan-1.jpg',
+  //   video: '',
+  //   price: 65,
+  //   description:
+  //     'Lentement fumé rehaussant ses saveurs naturelles pour un goût riche.',
+  //   available: true,
+  //   details:
+  //     'Le filet de flétan fumé a une chair de couleur blanche. Les arômes légers de notre fumage préservent la saveur iodée et la texture légère et fondant. Coupé à la perfection, nos steaks de flétan témoignent de la compétence de nos maîtres poissonniers.',
+  //   consumption:
+  //     'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
+  // },
+  // {
+  //   id: 11,
+  //   url: 'truite',
+  //   name: 'K - Truites Fumées',
+  //   shortName: 'K - Truites',
+  //   img: 'truite-1.jpeg',
+  //   imgUrl: 'truite/truite-1.jpeg',
+  //   video: '',
+  //   price: 24,
+  //   description: "Truite fumée au bois d'hêtre puis filetée manuellement.",
+  //   available: true,
+  //   details:
+  //     "Légère et savoureuse, la truite fumée peut se servir froide ou chaude. 4 paquets de 125g. Elle convient pour 2 personnes en petite entrée, ou pour une personne en plat ou entrée copieuse. Vous pouvez aussi présenter une entrée mixte, composée d’un filet de truite fumée et d’une tranche de saumon fumé. Nom latin: Oncorhynchus mykiss. Fermes certifiées ASC soucieuses de l'environnement et du bien-être animal Anciennes méthodes de fumage sur copeaux de bois de hêtre. Aucune utilisation d’additifs – uniquement du sel",
+  //   consumption:
+  //     'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. Prêt à manger et pratique pour un déjeuner rapide ou un repas chaud. Peut être dégusté froid ou chaud. Excellente source de protéines',
+  // },
+  // {
+  //   id: 12,
+  //   url: 'crevettes',
+  //   name: 'L - Crevettes Décortiquées',
+  //   shortName: 'L - Crevettes',
+  //   img: 'crevettes-1.jpg',
+  //   imgUrl: 'crevettes/crevettes-1.jpg',
+  //   video: '/crevettes/crevettes.mp4',
+  //   price: 18,
+  //   description:
+  //     'Soigneusement sélectionnées et préparées, délicieuses crevettes témoins de qualité.',
+  //   available: true,
+  //   details:
+  //     'Les crevettes sont cuites et décortiquées. 450g net. Elles sont ensuite fumées à froid avec du bois de hêtre. Les crevettes fumées sont ensuite conditionnées sous vide pour une meilleure conservation. Les crevettes fumées sont à conserver au frais entre 0 et 4°C. Elles se conservent 10 jours après ouverture.',
+  //   consumption:
+  //     'Consommer dans les 10 jours après ouverture. Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation.  Produit frais – peut être congelé. Conditionné sous vide pour une meilleure conservation. ',
+  // },
+  // {
+  //   id: 13,
+  //   url: 'couteau',
+  //   name: 'M - Couteau Saumon',
+  //   shortName: 'M - Couteau',
+  //   img: 'couteau.png',
+  //   imgUrl: 'couteau/couteau.png',
+  //   video: '',
+  //   price: 5,
+  //   description:
+  //     'Couteau à saumon. Lame arrondie pour une coupe nette et précise.',
+  //   available: true,
+  //   details:
+  //     'Couteau à saumon. Lame arrondie pour une coupe nette et précise. Longueure 30cm.',
+  //   consumption: '',
+  // },
 ];
