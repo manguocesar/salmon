b24f1bbb66c8095a8d6eee8c47dd6711d2f2f22d
Cesar <cesar.hertz@icloud.com>
2025-03-03 13:26:14 +0100

    Add delivery constants for testing free and paid shipping options

---
+++ src/app/constants/deliveries.ts
@@ -0,0 +1,17 @@
+export const testFreeDelivery = [
+  { shipping_rate: 'shr_1QuerSAZSYffeW1tgi7xnZid' }, //Mercredi 16 Avril de 12 & 18 heure collecte gratuite à Saint Cyr
+  { shipping_rate: 'shr_1Qufi0AZSYffeW1tF3iU2018' }, //Commande inférieure à 250€ - livraison Jeudi 17 Avril 2025
+];
+
+export const testPaidDelivery = [
+  { shipping_rate: 'shr_1QuerSAZSYffeW1tgi7xnZid' }, //Mercredi 16 Avril de 12 & 18 heure collecte gratuite à Saint Cyr
+  { shipping_rate: 'shr_1QufiQAZSYffeW1txqW3r6nz' }, //Commande supérieur à 250€ - livraison gratuite Jeudi 17 Avril 2025
+];
+
+export const liveFreeDelivery = [
+  { shipping_rate: 'shr_1QyXnbAZSYffeW1t3Vyvwdh6' }, //Mercredi 16 ou Jeudi 18 AVRIL Métropole de Lyon >250€
+];
+
+export const livePaidDelivery = [
+  { shipping_rate: 'shr_1QyXosAZSYffeW1tHzMHo9GM' }, //Mercredi 16 ou Jeudi 18 AVRIL Métropole de Lyon - inférieur à 250€
+];
