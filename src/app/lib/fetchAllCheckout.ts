4c573ee234383597ce8438bfc7ad033019106a22
Cesar <cesar.hertz@icloud.com>
2025-03-07 20:23:03 +0100

    Refactor fetchAllCheckoutSessions to enforce type safety and update Stripe API version

--- src/app/lib/fetchAllCheckout.ts
+++ src/app/lib/fetchAllCheckout.ts
@@ -1,13 +1,15 @@
 import Stripe from 'stripe';
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripeSecretKey: string | undefined = process.env.STRIPE_SECRET_KEY;
 if (!stripeSecretKey) {
   throw new Error('Stripe secret key is not defined');
 }
-const stripe = new Stripe(stripeSecretKey);
+const stripe = new Stripe(stripeSecretKey, { apiVersion: '2025-02-24.acacia' });
 
-export const fetchAllCheckoutSessions = async () => {
-  let allSessions: Stripe.Checkout.Session[] = [];
+export const fetchAllCheckoutSessions = async (): Promise<
+  Stripe.Checkout.Session[]
+> => {
+  const allSessions: Stripe.Checkout.Session[] = [];
   let hasMore = true;
   let lastSessionId: string | undefined = undefined;
 
@@ -24,13 +26,14 @@ export const fetchAllCheckoutSessions = async () => {
     const sessions = await stripe.checkout.sessions.list(params);
 
     hasMore = sessions.has_more;
-    allSessions = [...allSessions, ...sessions.data];
+    allSessions.push(...sessions.data);
 
     if (sessions.data.length > 0) {
       lastSessionId = sessions.data[sessions.data.length - 1].id;
     } else {
       hasMore = false;
     }
+
     const safetyLimit = 10000;
     if (allSessions.length > safetyLimit) {
       console.warn('Reached safety limit of 10,000 sessions');
